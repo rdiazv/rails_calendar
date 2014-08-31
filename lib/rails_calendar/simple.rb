a52aa43026641e98a61591f9fcd4264d64a432b5
Rodrigo Díaz <rdiaz@goplaceit.com>
2014-08-31 13:29:11 -0400

    Rendering calendar header

--- lib/rails_calendar/simple.rb
+++ lib/rails_calendar/simple.rb
@@ -1,5 +1,20 @@
+require 'action_view'
+
 module RailsCalendar
   class Simple
+    include ActionView::Helpers
+    include ActionView::Context
 
+    private
+
+    def header
+      content_tag :thead do
+        content_tag :tr do
+          I18n.t('date.abbr_day_names').map do |day|
+            content_tag :th, day.titleize
+          end.join.html_safe
+        end
+      end
+    end
   end
 end
--- spec/rails_calendar/simple_spec.rb
+++ spec/rails_calendar/simple_spec.rb
@@ -1,2 +1,27 @@
-describe RailsCalendar::Simple do
+I18n.backend.store_translations :test,
+  date: {
+    abbr_day_names: [
+      'A', 'B', 'C', 'D', 'E', 'F', 'G'
+    ]
+  }
+
+I18n.locale = :test
+
+describe RailsCalendar::Simple, type: :feature do
+  describe '#header' do
+    before do
+      calendar = RailsCalendar::Simple.new
+      @header = calendar.send(:header)
+    end
+
+    it 'should render a th tag for every day of the week' do
+      expect(@header).to have_selector('thead > tr > th', count: 7)
+    end
+
+    it 'should set the names of the days in each th' do
+      I18n.t('date.abbr_day_names').each do |day|
+        expect(@header).to have_selector('th', text: day.titleize)
+      end
+    end
+  end
 end
